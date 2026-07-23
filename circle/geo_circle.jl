92a8df034e8295b038247148b67d0e7fd70e9778
Aki <zyl99@mail.nankai.edu.cn>
2026-07-23 14:23:31 +0200

    Write down the geo file for rectangle case

--- circle/geo_circle.jl
+++ circle/geo_circle.jl
@@ -2,28 +2,28 @@ using GridapGmsh: gmsh
 
 gmsh.initialize()
 
-gmsh.model.add("Geo")
+gmsh.model.add("Geo_circle")
 
 lc=0.05
 
-p1 = gmsh.model.geo.addPoint(1,0,0,lc)
-p2 = gmsh.model.geo.addPoint(0,1,0,lc)
-p3 = gmsh.model.geo.addPoint(-1,0,0,lc)
-p4 = gmsh.model.geo.addPoint(0,-1,0,lc)
-center = gmsh.model.geo.addPoint(0,0,0,lc)
+p1 = gmsh.model.geo.addPoint(1, 0, 0, lc)
+p2 = gmsh.model.geo.addPoint(0, 1, 0, lc)
+p3 = gmsh.model.geo.addPoint(-1, 0, 0, lc)
+p4 = gmsh.model.geo.addPoint(0, -1, 0, lc)
+center = gmsh.model.geo.addPoint(0, 0, 0, lc)
 
-l1 = gmsh.model.geo.addCircleArc(p1,center,p2)
-l2 = gmsh.model.geo.addCircleArc(p2,center,p3)
-l3 = gmsh.model.geo.addCircleArc(p3,center,p4)
-l4 = gmsh.model.geo.addCircleArc(p4,center,p1)
+l1 = gmsh.model.geo.addCircleArc(p1, center, p2)
+l2 = gmsh.model.geo.addCircleArc(p2, center, p3)
+l3 = gmsh.model.geo.addCircleArc(p3, center, p4)
+l4 = gmsh.model.geo.addCircleArc(p4, center, p1)
 
-circle = gmsh.model.geo.addCurveLoop([l1,l2,l3,l4])
+circle = gmsh.model.geo.addCurveLoop([l1, l2, l3, l4])
 disk = gmsh.model.geo.addPlaneSurface([circle])
 
 gmsh.model.geo.synchronize()
 
-gmsh.model.addPhysicalGroup(1,[l1,l2,l3,l4],-1,"boundary")
-gmsh.model.addPhysicalGroup(2,[disk],-1,"domain")
+gmsh.model.addPhysicalGroup(1, [l1, l2, l3, l4], -1, "boundary")
+gmsh.model.addPhysicalGroup(2, [disk], -1, "domain")
 
 gmsh.model.mesh.generate(2)
 
--- rectangle/geo_rectangle.jl
+++ rectangle/geo_rectangle.jl
@@ -1,2 +1,35 @@
 using GridapGmsh: gmsh
 
+gmsh.initialize()
+
+gmsh.model.add("Geo_rectangle")
+
+lc=0.05
+
+p1 = gmsh.model.geo.addPoint(0, 0, 0, lc)
+p2 = gmsh.model.geo.addPoint(pi, 0, 0, lc)
+p3 = gmsh.model.geo.addPoint(pi, pi, 0, lc)
+p4 = gmsh.model.geo.addPoint(0, pi, 0, lc)
+
+l1 = gmsh.model.geo.addLine(p1, p2) 
+l2 = gmsh.model.geo.addLine(p2, p3)
+l3 = gmsh.model.geo.addLine(p3, p4)
+l4 = gmsh.model.geo.addLine(p4, p1)
+
+rectangle = gmsh.model.geo.addCurveLoop([l1, l2, l3, l4])
+rectangle_area = gmsh.model.geo.addPlaneSurface([rectangle])
+
+gmsh.model.geo.synchronize()
+
+gmsh.model.addPhysicalGroup(1, [l1, l2, l3, l4], -1, "boundary")
+gmsh.model.addPhysicalGroup(2, [rectangle_area], -1, "domain")
+
+gmsh.model.mesh.generate(2)
+
+gmsh.write(joinpath(@__DIR__, "geo-rectangle.msh"))
+
+if !("-nopopup" in ARGS)
+    gmsh.fltk.run()
+end
+
+gmsh.finalize()
